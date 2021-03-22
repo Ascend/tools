c3af1443547b2a61a4e8aabea1640748c8302ced
tan-qinlin <tanqinlin@huawei.com>
2021-03-22 15:45:36 +0800

    modify,net_compare.py

--- msquickcmp/compare/net_compare.py
+++ msquickcmp/compare/net_compare.py
@@ -13,7 +13,7 @@ import subprocess
 from common import utils
 from common.utils import AccuracyCompareException
 
-MSACCUCMP_PATH = "toolkit/tools/operator_cmp/compare/msaccucmp.pyc"
+MSACCUCMP_PATH = "toolkit/tools/operator_cmp/compare/msaccucmp.py"
 
 
 class NetCompare(object):
