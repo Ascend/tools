9a6d9022b5d2f74ab29202d8ba4bd938875453e1
刘超越 <466015832@qq.com>
2021-04-13 11:11:45 +0800

    update makesd/for_beta/make_ubuntu_sd.sh.

--- makesd/for_beta/make_ubuntu_sd.sh
+++ makesd/for_beta/make_ubuntu_sd.sh
@@ -413,7 +413,6 @@ network:
     usb0:
       dhcp4: no 
       addresses: [${USB_CARD_DEFAULT_IP}/24] 
-      #gateway4: ${NETWORK_CARD_GATEWAY}
 \" > /etc/netplan/01-netcfg.yaml
 echo \"config network ok\"
 # 5. auto-run minirc_cp.sh and minirc_sys_init.sh when start ubuntu
